7ff783f9e597b8878209f816ef89889e1c6c2f2a
Khatuna <you@example.com>
2022-06-28 19:43:57 +0200

    exercise 8 and 9

--- src/components/Budgets.js
+++ src/components/Budgets.js
@@ -3,48 +3,44 @@ import { useState, useEffect } from "react"
 
 export default function Budgets({ budgetData }) {
     const [actualState, setActualState] = useState([]);
+    const [searchInput, setSearchInput] = useState("")
 
     useEffect(() => {
         setActualState([...budgetData])
     }, [budgetData])
 
+    function sortAZ() {
+        let newBudgetList = [...actualState];
+        newBudgetList.sort((a, b) => a.budgetName.toLowerCase() > b.budgetName.toLowerCase() ? 1 : -1);
 
-
-
-
-    function sortByAlphabet() {
-
-        /* setActualState(prev => {
-             return prev.sort((a, b) =>
-                 a.budgetName.toLowerCase() > b.budgetName.toLowerCase() ? 1 : -1)
-         })*/
-
-        let newBudgetList = [...actualState]
-        newBudgetList.sort((a, b) => a.budgetName.toLowerCase() > b.budgetName.toLowerCase() ? 1 : -1)
-
-        setActualState(newBudgetList)
-
-        /* setActualState(prev => {
-            return prev.sort((a, b) => {
-                if (a.budgetName.toLowerCase() > b.budgetName.toLowerCase()) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            })
-        }) */
-
-
+        setActualState(newBudgetList);
     }
+
+    function sortByDate() {
+        let newBudgetList = [...actualState];
+        newBudgetList.sort((a, b) => a.date.toLowerCase() > b.date.toLowerCase() ? 1 : -1);
+
+        setActualState(newBudgetList);
+    }
+
+    function reIninitialize() {
+        setActualState(prev => prev = budgetData);
+    }
+
+   function searchByBudgetName() {
+        let newBudgetList = [...actualState];
+        setActualState(prev => prev = newBudgetList.filter(item => item.budgetName === searchInput));
+   }
 
     return (
         <div className="BUDGET_LIST_CONTAINER">
-            <div style={{ width: "100%", height: "50px" }}>
-                <button style={{ height: "20px" }} onClick={sortByAlphabet}>ordena A-Z</button>
+            <div className="BUDGET_NAV">
+                <button style={{ height: "20px" }} onClick={sortAZ}>Ordena A-Z</button>
+                <button style={{ height: "20px" }} onClick={sortByDate}>Ordena por data</button>
+                <button style={{ height: "20px" }} onClick={reIninitialize}>Reicializar orden</button>
+                <input value={searchInput} onChange={e => setSearchInput(e.target.value)} />
+                <button style={{ height: "20px" }} onClick={searchByBudgetName}>Buscar por porsupuesto</button>
             </div>
-
-
-
 
             {actualState.map((item, index) => {
                 return <BudgetItem
